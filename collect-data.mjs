3d6e4a40b94af92873b828307cf05d3e6a36e547
Dominique Hazael-Massieux <dom@w3.org>
2021-11-19 19:32:10 +0100

    Migrate to load IDL implementation data via es module

--- collect-data.mjs
+++ collect-data.mjs
@@ -1,15 +1,16 @@
-const wg = require("./lib/wg");
-const repo = require("./lib/repo");
-const wpt = require("./lib/wpt");
-const idl = require("./lib/idl");
-const idlImpl = require("./lib/idl-impl");
+import wg from "./lib/wg.mjs";
+import repo from "./lib/repo.mjs";
+import wpt from "./lib/wpt.mjs";
+import {calculateSurface} from "./lib/idl.mjs";
+import idlImpl from "./lib/idl-impl.mjs";
+import fs from "fs";
 
 // WG id
 const gid = process.argv[2];
 
 
 // WG configurations
-const wgConfig = require("./group-config.json");
+const wgConfig = JSON.parse(fs.readFileSync("./group-config.json", "utf-8"))
 
 const results = {specifications:[], config: wgConfig[gid]};
 
@@ -17,7 +18,7 @@ const results = {specifications:[], config: wgConfig[gid]};
   results.specifications = await wg.listSpecs(gid);
   results.specifications = await Promise.all(results.specifications.map(async (spec) => {
     // IDL surface
-    spec.idl = await idl.calculateSurface(spec.shortname);
+    spec.idl = await calculateSurface(spec.shortname);
 
     // Implementations
     spec.implementationGaps = [];
